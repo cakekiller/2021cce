249495746dd37970125d56cb9231fdb027a36a4d
cakekiller <91170824+cakekiller@users.noreply.github.com>
2021-12-29 11:56:21 +0800

    Add files via upload

---
+++ week16/week16-2.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int line[100];
+int main()
+{
+    printf("Please input a line: ");
+    scanf("%s",line);
+
+    printf("%s\n",line);
+}
---
+++ week16/week16-5.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+char line[300]="10101000001000101110110011111";
+int main()
+{
+    int ans=0;
+
+    for(int i=0;line[i]!='\0';i++)
+    {
+
+        if(line [i] == '1')ans++;
+
+
+    }
+    printf("¦³%d­Ó1\n",ans);
+}
+
+
